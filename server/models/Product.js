25ddf79a93dd3b799e9419c387a8c185c0de6730
kodamulla <hansikavithma25@gmail.com>
2026-06-18 18:40:27 +0530

    Setup backend server, MongoDB connection, and Product model

---
+++ server/models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true, // e.g., T-Shirts, Jeans, Dresses
+    },
+    sizes: {
+        type: [String], // e.g., ['S', 'M', 'L', 'XL']
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        default: 0,
+    }
+}, {
+    timestamps: true // Item eka add karapu/update karapu welawa auto save wenawa
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
